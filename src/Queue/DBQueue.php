0b918699ae0829e607cf8dcf0908e00d4b246004
karunais <karunais1392@gmail.com>
2021-09-02 09:33:27 +0800

    -Fix weird issue when delete job

--- src/Queue/DBQueue.php
+++ src/Queue/DBQueue.php
@@ -6,17 +6,13 @@ use Illuminate\Queue\DatabaseQueue;
 
 class DBQueue extends DatabaseQueue
 {
-    
+
     /**
      * {@inheritdoc}
      */
     public function deleteReserved($queue, $id)
     {
-
-        if( !method_exists($this->database, 'getPdo') ) {
-            $this->database = \DB::connection('currentHost');
-        }
-
+        $this->database = $this->database->getPdo() ?? \DB::connection($this->database->getConfig('name'));
 
         parent::deleteReserved($queue, $id);
     }
