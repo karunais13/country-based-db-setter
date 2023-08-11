3ee2e2f54618a73287c6733e73f3f8aad87b65aa
Karunais13 <karunais1392@gmail.com>
2023-08-12 01:22:14 +0800

    Fix missing connection when process job

--- src/Queue/DBQueue.php
+++ src/Queue/DBQueue.php
@@ -6,13 +6,86 @@ use Illuminate\Queue\DatabaseQueue;
 
 class DBQueue extends DatabaseQueue
 {
+    /**
+     * Get the size of the queue.
+     *
+     * @param  string|null  $queue
+     * @return int
+     */
+    public function size($queue = null)
+    {
+        $this->resetConnection();
+
+        return parent::size($queue);
+    }
+
+    public function bulk($jobs, $data = '', $queue = null)
+    {
+        $this->resetConnection();
+
+        return parent::bulk($jobs, $data, $queue);
+    }
+
+    /**
+     * Push a raw payload to the database with a given delay.
+     *
+     * @param  string|null  $queue
+     * @param  string  $payload
+     * @param  \DateTimeInterface|\DateInterval|int  $delay
+     * @param  int  $attempts
+     * @return mixed
+     */
+    protected function pushToDatabase($queue, $payload, $delay = 0, $attempts = 0)
+    {
+        $this->resetConnection();
+
+        return parent::pushToDatabase($queue, $payload, $delay, $attempts);
+    }
+
+    /**
+     * Get the next available job for the queue.
+     *
+     * @param  string|null  $queue
+     * @return \Illuminate\Queue\Jobs\DatabaseJobRecord|null
+     */
+    protected function getNextAvailableJob($queue)
+    {
+        $this->resetConnection();
+
+        return parent::getNextAvailableJob($queue);
+    }
+
+    /**
+     * Get the lock required for popping the next job.
+     *
+     * @return string|bool
+     */
+    protected function getLockForPopping()
+    {
+        $this->resetConnection();
+
+        return parent::getLockForPopping();
+    }
+
+    /**
+     * Mark the given job ID as reserved.
+     *
+     * @param  \Illuminate\Queue\Jobs\DatabaseJobRecord  $job
+     * @return \Illuminate\Queue\Jobs\DatabaseJobRecord
+     */
+    protected function markJobAsReserved($job)
+    {
+        $this->resetConnection();
+
+        return parent::markJobAsReserved($job);
+    }
 
     /**
      * {@inheritdoc}
      */
     public function deleteReserved($queue, $id)
     {
-        $this->database = $this->database->getPdo() ?? \DB::connection($this->database->getConfig('name'));
+        $this->resetConnection();
 
         parent::deleteReserved($queue, $id);
     }
@@ -28,5 +101,14 @@ class DBQueue extends DatabaseQueue
         }
 
         return parent::pop($queue);
+    }
+
+
+    private function resetConnection()
+    {
+        if( (config('queue.default') === 'database' ) && !($this->database instanceof MySqlConnection) ){
+            $this->database = \DB::connection($this->database->getConfig('name'));
+        }
+
     }
 }
